9955dbd8dcf053e420b661c61a657bfd6dc02278
Madhu <madhu8129@gmail.com>
2024-04-17 14:38:50 +0200

    installed hook-react-form

---
+++ ReactJS-hooks/hooks-practice/src/Skill.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Skill = () => {
+    return (
+        <>
+           <ol>Skill table
+           <li>Java</li>
+           <li>Spring Boot</li>
+           <li>React</li>
+           <li>SQL</li>
+           </ol>
+           <ul>Skill table
+           <li>Java</li>
+           <li>Spring Boot</li>
+           <li>React</li>
+           <li>SQL</li>
+           <p>Java</p>
+           </ul>
+
+        </>
+ 
+    );
+};
+
+export default Skill;
